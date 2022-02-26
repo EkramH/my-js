e6fc9af294c41de3bd3dc7dbbbd5a7d7140b4b02
EkramH <ekramhosen329@gmail.com>
2022-02-26 23:49:51 +0600

    geting element by id

---
+++ goribi-bank/script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    //Get user email
+    const usersEmail = document.getElementById('user-email').value;
+    
+    // Get user password
+    const usersPassword = document.getElementById('user-password').value;
+
+    
+})
