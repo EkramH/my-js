f83ed952c5869ade948e9d7d7203000711c5d8a5
EkramH <ekramhosen329@gmail.com>
2022-03-14 23:05:02 +0600

    weather icon

--- weather app/script.js
+++ weather app/script.js
@@ -17,5 +17,8 @@ const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
     console.log(temperature);
 }
