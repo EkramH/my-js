68d344d173f976b6b9afc06a798afbd03b910f54
Ekram Hosen <ekramhosen329@gmail.com>
2022-06-23 12:51:22 +0600

    table and content update from json

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("projectList.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (products) {
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
+    for (let product of products) {
+      out += `
+         <tr class="${product.bgColor} border-b">
+            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left hover:text-teal-600">${product.index}</td>
+            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlLive}">${product.name}</a></td>
+            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlCode}">${product.codeBase}</a> </td>
+         </tr>
+      `;
+    }
+
+    placeholder.innerHTML = out;
+  });
