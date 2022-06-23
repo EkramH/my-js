857f3418142b653f388bb935fa0a9756d930d996
Ekram Hosen <ekramhosen329@gmail.com>
2022-06-23 13:10:57 +0600

    link update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ fetch("projectList.json")
       out += `
          <tr class="${product.bgColor} border-b">
             <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left hover:text-teal-600">${product.index}</td>
-            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlLive}">${product.name}</a></td>
-            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlCode}">${product.codeBase}</a> </td>
+            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlLive}" target="_blank">${product.name}</a></td>
+            <td class="text-lg font-medium text-gray-900 px-6 py-4 text-left"><a href="${product.urlCode}" target="_blank">${product.codeBase}</a></td>
          </tr>
       `;
     }
